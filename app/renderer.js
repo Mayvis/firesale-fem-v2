b673f1f0a549f85ec81b048b4801592b4649e529
Mayvis <kevin73911@gmail.com>
2021-06-30 23:35:06 +0800

    add edited

--- app/renderer.js
+++ app/renderer.js
@@ -1,6 +1,11 @@
 const marked = require("marked");
+const path = require("path");
 const { remote, ipcRenderer } = require("electron");
 const mainProcess = remote.require("./main");
+const currentWindow = remote.getCurrentWindow();
+
+let filePath = null;
+let originalContent = "";
 
 const markdownView = document.querySelector("#markdown");
 const htmlView = document.querySelector("#html");
@@ -16,9 +21,27 @@ const renderMarkdownToHtml = (markdown) => {
   htmlView.innerHTML = marked(markdown, { sanitize: true });
 };
 
+const updateUserInterface = (isEdited) => {
+  let title = "Fire Sale";
+
+  if (filePath) {
+    title = `${path.basename(filePath)} - ${title}`;
+
+    title += isEdited ? " (Edited)" : "";
+  }
+
+  saveMarkdownButton.disabled = !isEdited;
+  revertButton.disabled = !isEdited;
+
+  currentWindow.setTitle(title);
+};
+
 markdownView.addEventListener("keyup", (event) => {
   const currentContent = event.target.value;
+
   renderMarkdownToHtml(currentContent);
+
+  updateUserInterface(currentContent !== originalContent);
 });
 
 openFileButton.addEventListener("click", () => {
@@ -26,6 +49,11 @@ openFileButton.addEventListener("click", () => {
 });
 
 ipcRenderer.on("file-opened", (event, file, content) => {
+  filePath = file;
+  originalContent = content;
+
   markdownView.value = content;
   renderMarkdownToHtml(content);
+
+  updateUserInterface(false);
 });
