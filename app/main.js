6db1facf7563fcc8757b73388ec51ad2ea08f368
Mayvis <kevin73911@gmail.com>
2021-06-30 23:11:29 +0800

    emit ipc event and open dialog

--- app/main.js
+++ app/main.js
@@ -1,1 +1,39 @@
-console.log('This is an Electron application!');
+const { app, BrowserWindow, dialog } = require("electron");
+const fs = require("fs");
+
+let mainWindow = null;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow();
+  mainWindow.loadFile(`${__dirname}/index.html`);
+
+  mainWindow.once("ready-to-show", () => {
+    mainWindow.show();
+  });
+});
+
+exports.getFileFromUser = () => {
+  const files = dialog.showOpenDialog({
+    properties: ["openFile"],
+    // buttonLabel: "Unveil",
+    // title: "Open Fire Sale Document",
+    filters: [
+      { name: "Markdown Files", extensions: ["md", "markdown"] },
+      { name: "Text Files", extensions: ["txt", "text"] },
+    ],
+  });
+
+  // handle files is undefined situation
+  if (!files) return;
+
+  const file = files[0];
+
+  openFile(file);
+};
+
+const openFile = (filePath) => {
+  const content = fs.readFileSync(filePath).toString();
+
+	// emit information to IPC (Interprocess communication)
+  mainWindow.webContents.send("file-opened", filePath, content);
+};
